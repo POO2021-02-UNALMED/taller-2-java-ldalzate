5cd2a3ed1d6c4c79e6b0e93d569ff3c001544cca
ldalzate <ldalzatef@unal.edu.co>
2021-10-19 21:36:22 -0500

    Corrección del código

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -2,34 +2,42 @@ package test;
 
 public class Auto {
 
-	public String modelo, marca;
-	public int precio, registro;
-	public static int cantidadCreados;
-	public Asiento[] asientos;
-	public Motor motor;
+    public String modelo, marca;
+    public int precio, registro;
+    public static int cantidadCreados;
+    public Asiento[] asientos;  // {1, null, 2, null}
+    public Motor motor;
 
-	public int cantidadAsientos() {
-		return asientos.length;
-	}
+    public int cantidadAsientos() {
+        int cantidadAsientos = 0;
+        for (Asiento asiento: asientos){
+            
+            if (asiento != null){
+                cantidadAsientos ++;
+            }
+        }
+        return cantidadAsientos;
+    }
 
-	public String verificarIntegridad() {
-
-		// Verificación de asientos
-		boolean asientosOriginales = true;
-
-		for (Asiento asiento : asientos) {
-
-			if (asiento != null && asiento.registro != motor.registro) {
-				asientosOriginales = false;
-				break;
-			}
-		}
-
-		// Verificar la originalidad de las demás partes
-		if (motor.registro == registro && asientosOriginales) {
-			return "Autor original";
-		} else {
-			return "Las piezas no son originales";
-		}
-	}
+    public String verificarIntegridad() {
+        
+        // Verificación de asientos
+        boolean asientosOriginales = true; 
+        
+        for (Asiento asiento: asientos){
+            
+            if (asiento != null && asiento.registro != motor.registro){
+                asientosOriginales = false;
+                break;
+            }
+        }
+        
+        // Verificar la originalidad de las demás partes
+        if (motor.registro == registro && asientosOriginales){
+            return "Auto original";        
+        }
+        else {
+            return "Las piezas no son originales";
+        }
+    }
 }
